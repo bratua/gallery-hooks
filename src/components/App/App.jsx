cde67c05ab55153363de87084765c2f531c6ef8a
Sergey Lisoviy <serlipaua@gmail.com>
2022-12-22 11:54:21 +0200

    refacroting app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import Box from 'components/Box';
-import { PureComponent } from 'react';
+import { PureComponent, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import * as API from 'API_Pixabay/API_Pixabay';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,181 +7,193 @@ import { SearchBar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
 import { Button } from 'components/Button';
 
-export class App extends PureComponent {
-  state = {
-    searchQuery: '',
-    pictures: [],
-    picturesCount: 0,
-    page: 1,
-    progress: 'idle',
-    imagesLeft: null,
-    imagesInQuery: null,
-    scrollToId: null,
-    statsQuery: null,
-  };
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pictures, setPictures] = useState([]);
+  const [picturesCount, setPicturesCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const [progress, setProgress] = useState('idle');
+  const [imagesLeft, setImagesLeft] = useState(null);
+  const [imagesInQuery, setImagesInQuery] = useState(null);
+  const [scrollToId, setScrollToId] = useState(null);
+  const [statsQuery, setStatsQuery] = useState(null);
+};
 
-  componentDidMount() {
-    this.setState({ searchQuery: '' });
-  }
+// export class App extends PureComponent {
+//   state = {
+//     searchQuery: '',
+//     pictures: [],
+//     picturesCount: 0,
+//     page: 1,
+//     progress: 'idle',
+//     imagesLeft: null,
+//     imagesInQuery: null,
+//     scrollToId: null,
+//     statsQuery: null,
+//   };
 
-  async componentDidUpdate(prevProps, prevState) {
-    // console.log('upp start');
-    if (this.state.page !== prevState.page) {
-      await this.getPictures();
-      await this.scrollNextPage();
-      await this.showStats();
+//   componentDidMount() {
+//     this.setState({ searchQuery: '' });
+//   }
 
-      // this.onLoad();
-    }
-    // console.log('upp end');
+//   async componentDidUpdate(prevProps, prevState) {
+//     // console.log('upp start');
+//     if (this.state.page !== prevState.page) {
+//       await this.getPictures();
+//       await this.scrollNextPage();
+//       await this.showStats();
 
-    if (this.state.searchQuery !== prevState.searchQuery) {
-      this.setState({ pictures: [], page: 1 });
-      await this.getPictures();
-      this.showStats();
-    }
-  }
+//       // this.onLoad();
+//     }
+//     // console.log('upp end');
 
-  onSearch = query => {
-    if (query === this.state.searchQuery) {
-      return;
-    }
-    this.setState({ searchQuery: query });
-  };
+//     if (this.state.searchQuery !== prevState.searchQuery) {
+//       this.setState({ pictures: [], page: 1 });
+//       await this.getPictures();
+//       this.showStats();
+//     }
+//   }
 
-  onNext = () => {
-    this.setState(prev => ({
-      page: prev.page + 1,
-      imagesLeft: prev.imagesInQuery - this.state.page * 12,
-    }));
-  };
+//   onSearch = query => {
+//     if (query === this.state.searchQuery) {
+//       return;
+//     }
+//     this.setState({ searchQuery: query });
+//   };
 
-  onLoad = () => {
-    setTimeout(() => {
-      if (this.state.picturesCount === 0) {
-        this.setState({ progress: 'loaded' });
-      }
-    }, 0);
-  };
+//   onNext = () => {
+//     this.setState(prev => ({
+//       page: prev.page + 1,
+//       imagesLeft: prev.imagesInQuery - this.state.page * 12,
+//     }));
+//   };
 
-  onLoadImgCheck = loadStatus => {
-    // console.log(loadStatus);
-    if (loadStatus) {
-      this.setState(state => ({
-        picturesCount: state.picturesCount - 1,
-      }));
-      this.onLoad();
-      return;
-    }
-  };
+//   onLoad = () => {
+//     setTimeout(() => {
+//       if (this.state.picturesCount === 0) {
+//         this.setState({ progress: 'loaded' });
+//       }
+//     }, 0);
+//   };
 
-  getPictures = async () => {
-    // let { pictures, statsQuery } = this.state;
-    let pictures = [];
-    let statsQuery = null;
-    try {
-      this.setState({ progress: 'loading' });
-      const apiResponse = await API.getQueryPicture(
-        this.state.searchQuery,
-        this.state.page
-      );
-      pictures = apiResponse.hits;
-      statsQuery = apiResponse.stats;
+//   onLoadImgCheck = loadStatus => {
+//     // console.log(loadStatus);
+//     if (loadStatus) {
+//       this.setState(state => ({
+//         picturesCount: state.picturesCount - 1,
+//       }));
+//       this.onLoad();
+//       return;
+//     }
+//   };
 
-      if (pictures.length === 0) {
-        // console.log('No images!');
-        throw new Error(
-          `${this.props.searchQuery} Картинок по зпросу не найдено!`
-        );
-      }
-    } catch (error) {
-      // console.log('catch ', error.message);
-      toast.error(error.message, {
-        position: 'top-right',
-      });
+//   getPictures = async () => {
+//     // let { pictures, statsQuery } = this.state;
+//     let pictures = [];
+//     let statsQuery = null;
+//     try {
+//       this.setState({ progress: 'loading' });
+//       const apiResponse = await API.getQueryPicture(
+//         this.state.searchQuery,
+//         this.state.page
+//       );
+//       pictures = apiResponse.hits;
+//       statsQuery = apiResponse.stats;
 
-      this.setState({
-        errorMessage: error.message,
-        progress: 'idle',
-      });
-      return;
-    }
-    console.log('pictures', pictures);
-    console.log('statsQuery', statsQuery);
+//       if (pictures.length === 0) {
+//         // console.log('No images!');
+//         throw new Error(
+//           `${this.props.searchQuery} Картинок по зпросу не найдено!`
+//         );
+//       }
+//     } catch (error) {
+//       // console.log('catch ', error.message);
+//       toast.error(error.message, {
+//         position: 'top-right',
+//       });
 
-    this.setState(prevState => ({
-      pictures: [...prevState.pictures, ...pictures],
-      scrollToId: pictures[0].id,
-      picturesCount: pictures.length,
-      imagesInQuery: statsQuery,
-    }));
-  };
+//       this.setState({
+//         errorMessage: error.message,
+//         progress: 'idle',
+//       });
+//       return;
+//     }
+//     console.log('pictures', pictures);
+//     console.log('statsQuery', statsQuery);
 
-  showStats = async () => {
-    setTimeout(() => {
-      let totalPages = Math.ceil(this.state.imagesInQuery / 12);
-      let pagesLeft = totalPages - this.state.page;
+//     this.setState(prevState => ({
+//       pictures: [...prevState.pictures, ...pictures],
+//       scrollToId: pictures[0].id,
+//       picturesCount: pictures.length,
+//       imagesInQuery: statsQuery,
+//     }));
+//   };
 
-      if (this.state.pictures.length > 0) {
-        toast.success(
-          `Найдено:
-          изображений: ${this.state.imagesInQuery};
-          страниц: ${totalPages};
-          страниц осталось: ${pagesLeft};
-          вы на: ${this.state.page} странице;`,
-          {
-            position: 'top-right',
-          }
-        );
-      }
-    }, 0);
-  };
+//   showStats = async () => {
+//     setTimeout(() => {
+//       let totalPages = Math.ceil(this.state.imagesInQuery / 12);
+//       let pagesLeft = totalPages - this.state.page;
 
-  scrollNextPage = async () => {
-    setTimeout(() => {
-      const target = this.state.scrollToId;
-      const targetItem = document.getElementById(`${target}`);
-      targetItem.scrollIntoView();
-    }, 0);
-  };
+//       if (this.state.pictures.length > 0) {
+//         toast.success(
+//           `Найдено:
+//           изображений: ${this.state.imagesInQuery};
+//           страниц: ${totalPages};
+//           страниц осталось: ${pagesLeft};
+//           вы на: ${this.state.page} странице;`,
+//           {
+//             position: 'top-right',
+//           }
+//         );
+//       }
+//     }, 0);
+//   };
 
-  render() {
-    return (
-      <Box className="App">
-        <SearchBar onSubmit={this.onSearch} />
+//   scrollNextPage = async () => {
+//     setTimeout(() => {
+//       const target = this.state.scrollToId;
+//       const targetItem = document.getElementById(`${target}`);
+//       targetItem.scrollIntoView();
+//     }, 0);
+//   };
 
-        <ImageGallery
-          pictures={this.state.pictures}
-          onLoadImg={this.onLoadImgCheck}
-        />
+//   render() {
+//     return (
+//       <Box className="App">
+//         <SearchBar onSubmit={this.onSearch} />
 
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
+//         <ImageGallery
+//           pictures={this.state.pictures}
+//           onLoadImg={this.onLoadImgCheck}
+//         />
 
-        {this.state.progress === 'loaded' && (
-          <Button
-            type="button"
-            className="Button"
-            disabled={
-              this.state.pictures.length < 12 ||
-              this.state.pictures.length % 12 !== 0
-            }
-            onClick={this.onNext}
-          >
-            Next
-          </Button>
-        )}
-      </Box>
-    );
-  }
-}
+//         <ToastContainer
+//           position="top-right"
+//           autoClose={5000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//           theme="dark"
+//         />
+
+//         {this.state.progress === 'loaded' && (
+//           <Button
+//             type="button"
+//             className="Button"
+//             disabled={
+//               this.state.pictures.length < 12 ||
+//               this.state.pictures.length % 12 !== 0
+//             }
+//             onClick={this.onNext}
+//           >
+//             Next
+//           </Button>
+//         )}
+//       </Box>
+//     );
+//   }
+// }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,77 +1,69 @@
-import { PureComponent } from 'react';
+import { useState } from 'react';
 import { PropTypes } from 'prop-types';
 import Box from 'components/Box';
 import { ImageLoader } from 'components/Loader';
 import { Modal } from 'components/Modal';
 
-export class ImageGalleryItem extends PureComponent {
-  static propType = {
-    imgUrl: PropTypes.string,
-    largeImageURL: PropTypes.string,
-    imgId: PropTypes.string,
-    tags: PropTypes.string,
-    onPreview: PropTypes.func,
-    onLoad: PropTypes.func,
-  };
+const ImageGalleryItem = ({ imgUrl, tags, largeImageURL, imgId, onLoad }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [pictureAlt, setPictureAlt] = useState('');
+  const [pictureLargeUrl, setPictureLargeUrl] = useState('');
+  // const [errorMessage, setErrorMessage] = useState(null);
 
-  state = {
-    loaded: false,
-    showModal: false,
-    pictureAlt: '',
-    pictureLargeUrl: '',
-    errorMessage: null,
-  };
-
-  checkImgLoad = event => {
+  const checkImgLoad = event => {
     const isImageLoaded = event.type === 'load';
 
     if (isImageLoaded) {
-      this.setState({ loaded: true });
-      this.props.onLoad(true);
+      setLoaded(true);
+      onLoad(true);
     }
   };
 
-  onPreview = (url, alt) => {
-    this.setState({ pictureAlt: alt, pictureLargeUrl: url });
-    this.toggleModal();
+  const onPreview = (url, alt) => {
+    setPictureLargeUrl(url);
+    setPictureAlt(alt);
+    toggleModal();
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
   };
 
-  render() {
-    const { imgUrl, tags, largeImageURL, imgId } = this.props;
-    const { showModal, pictureAlt, pictureLargeUrl } = this.state;
+  return (
+    <Box>
+      {showModal && (
+        <Modal url={pictureLargeUrl} alt={pictureAlt} close={toggleModal}>
+          <ImageLoader />
+        </Modal>
+      )}
 
-    return (
-      <Box>
-        {showModal && (
-          <Modal
-            url={pictureLargeUrl}
-            alt={pictureAlt}
-            close={this.toggleModal}
-          >
-            <ImageLoader />
-          </Modal>
-        )}
+      <li
+        className="GalleryItem"
+        id={imgId}
+        onClick={() => onPreview(largeImageURL, tags)}
+      >
+        <Box className="ImageWraper">
+          {!loaded && <ImageLoader />}
+          <img
+            className="ImageGalleryItem-image"
+            src={imgUrl}
+            alt={tags}
+            onLoad={checkImgLoad}
+          />
+        </Box>
+      </li>
+    </Box>
+  );
+};
 
-        <li
-          className="GalleryItem"
-          id={imgId}
-          onClick={() => this.onPreview(largeImageURL, tags)}
-        >
-          <Box className="ImageWraper">
-            {!this.state.loaded && <ImageLoader />}
-            <img
-              className="ImageGalleryItem-image"
-              src={imgUrl}
-              alt={tags}
-              onLoad={this.checkImgLoad}
-            />
-          </Box>
-        </li>
-      </Box>
-    );
-  }
-}
+export { ImageGalleryItem };
+
+ImageGalleryItem.propType = {
+  imgUrl: PropTypes.string,
+  largeImageURL: PropTypes.string,
+  imgId: PropTypes.string,
+  tags: PropTypes.string,
+  onPreview: PropTypes.func,
+  onLoad: PropTypes.func,
+};
